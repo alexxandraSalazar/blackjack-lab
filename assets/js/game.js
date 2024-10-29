0e66148bcae78ede86cec02a979d0f9e342e987c
Alexandra Salazar <138623509+Alex000127@users.noreply.github.com>
2024-10-28 21:30:41 -0600

    feat: ask for cards :sparkles:

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,13 +9,34 @@ let deck = [];
 const types = ['C', 'D', 'H', 'S']
 const special = ['A', 'J', 'Q', 'K']
 
+//This function creates a new deck 
 const createDeck = () => {
     for(let i = 2; i <= 10; i++){
         for(let type of types){
         deck.push(i + type);
         }
     }
+
+    for(let type of types){
+        for(let esp of special){
+            deck.push(esp + type)
+        }
+    }
+    deck = _.shuffle(deck);
     console.log(deck);
+    return deck;
 }
 
 createDeck();
+
+//This functions allows me to ask for a card
+
+const askCard = () =>{
+    if(deck.length === 0){
+        throw('no more cards in deck')
+    }
+    const card = deck.pop();
+
+    return card;
+}
+
