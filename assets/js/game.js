1e269411d111892a47b40bedbbe6b884474a4eaa
Alexandra Salazar <138623509+Alex000127@users.noreply.github.com>
2024-10-30 18:31:35 -0600

    feat: add missing functionality :sparkles:

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,12 @@
-/**
- * 2C = Two of clubs
- * 2D = Two of Diamonds
- * 2H = Two of Hearts
- * 2S = Two of Spades
-*/
-
-//Global variables
+// Global variables
 let deck = [];
-const types = ['C', 'D', 'H', 'S']
-const special = ['A', 'J', 'Q', 'K']
+const types = ['C', 'D', 'H', 'S'];
+const special = ['A', 'J', 'Q', 'K'];
 let playerPoints = 0,
     computerPoints = 0;
 let msg;
 
-// HTML's references
+// HTML references
 const btnAsk = document.querySelector('#btnAsk');
 const btnStop = document.querySelector('#btnStop');
 const btnNew = document.querySelector('#btnNew');
@@ -21,112 +14,120 @@ const pointsRef = document.querySelectorAll('small');
 const playerCards = document.querySelector('#player-cards');
 const computerCards = document.querySelector('#computer-cards');
 
-
-
-//This function creates a new deck 
+// Create a new deck function
 const createDeck = () => {
-    for(let i = 2; i <= 10; i++){
-        for(let type of types){
+    for (let i = 2; i <= 10; i++) {
+        for (let type of types) {
             deck.push(i + type);
         }
     }
-    
-    for(let type of types){
-        for(let esp of special){
-            deck.push(esp + type)
+
+    for (let type of types) {
+        for (let esp of special) {
+            deck.push(esp + type);
         }
     }
     deck = _.shuffle(deck);
     return deck;
-}
+};
 
 createDeck();
 
-//This functions allows me to ask for a card
-const askCard = () =>{
-    if(deck.length === 0){
-        throw('no more cards in deck')
-    }
-    const card = deck.pop();
-    return card;
-}
+// Ask for a card function
+const askCard = () => {
+    if (deck.length === 0) throw ('No more cards in deck');
+    return deck.pop();
+};
 
-//This funtions return the value of the card
-const cardValue = (card) =>{
+// Get card value function
+const cardValue = (card) => {
     const value = card.substring(0, card.length - 1);
-    return (isNaN(value)) ? ((value === 'A') ? 11 : 10) : parseInt(value) 
-}
+    return isNaN(value) ? (value === 'A' ? 11 : 10) : parseInt(value);
+};
 
-// Computers turn, return a message
-const computerTurn = (minPoints, msg) => {
-    do{
+// Computer's turn, returns a message
+const computerTurn = (minPoints) => {
+    do {
         const card = askCard();
-        computerPoints = computerPoints + cardValue(card);
-        pointsRef[1].innerHTML = computerPoints;
+        computerPoints += cardValue(card);
+        pointsRef[1].innerText = computerPoints;
+
         const newCard = document.createElement('img');
         newCard.src = `assets/cartas/${card}.png`;
         newCard.classList.add('cards');
         computerCards.append(newCard);
-        
-        if(minPoints > 21){
-            break;
-        }
-        
-    }while(minPoints > computerPoints && minPoints <= 21)
-        
-        setTimeout(() => {
-            if(computerPoints === minPoints){
-                msg = "It's a tie";
-            } else if(computerPoints > minPoints && computerPoints < 21){
-                 msg = "Computer wins";
-            } else{
-                 msg = "You win";
-            }
-        }, 1000);
 
-        return msg; 
+        if (minPoints > 21) break;
+
+    } while (computerPoints < minPoints && computerPoints <= 21);
+
+    if (computerPoints === minPoints) {
+        msg = "It's a tie";
+    } else if (computerPoints > minPoints && computerPoints <= 21) {
+        msg = "Computer wins";
+    } else {
+        msg = "computer win";
     }
-    
-    
-    
-    //Events
-    //Player turn, it returns a message
-    btnAsk.addEventListener('click',(msg) =>{
-        let message = msg;
-        const card = askCard();
-        playerPoints = playerPoints + cardValue(card);
-        pointsRef[0].innerText = playerPoints;
-        const newCard = document.createElement('img');
-        newCard.src = `assets/cartas/${card}.png`;
-        newCard.classList.add('cards');
-        playerCards.append(newCard);
-        
-        if(playerPoints > 21 ){
-            btnAsk.disabled = true;;
-            message = computerTurn(playerPoints, msg)
-            //player lost, is computer turn now
-        } else if (playerPoints === 21 ){
-            btnAsk.disabled = true;
-          //player wins
-        }
-        
-        // Victory message
-        if (message){
-            console.log(message);
-        } else {
-            console.log('msg in empty');
-        }
 
-    });
-    
-    btnStop.addEventListener('click', () => {
-        computerTurn(playerPoints);
+    return msg;
+};
+
+// Player turn
+btnAsk.addEventListener('click', () => {
+    const card = askCard();
+    playerPoints += cardValue(card);
+    pointsRef[0].innerText = playerPoints;
+
+    const newCard = document.createElement('img');
+    newCard.src = `assets/cartas/${card}.png`;
+    newCard.classList.add('cards');
+    playerCards.append(newCard);
+
+    if (playerPoints > 21) {
         btnAsk.disabled = true;
-        btnNew.disabled = true;
+        btnStop.disabled = true;
+        let showMsg;
+        showMsg = computerTurn(playerPoints);
+        Swal.fire({
+            icon: "error",
+            title: "You lost...",
+            text: showMsg
+        });
+    } else if (playerPoints === 21) {
+        btnAsk.disabled = true;
+        btnStop.disabled = true;
+        Swal.fire({
+            title: "Good job!",
+            text: "You win!",
+            icon: "success"
+        });
+    }
+});
+
+// Stop button
+btnStop.addEventListener('click', () => {
+    let showMsg;
+    showMsg = computerTurn(playerPoints);
+    title = (showMsg === "It's a tie") ? 'Nobody won' : "You lost...";
+    Swal.fire({
+        icon: "error",
+        title: title,
+        text: showMsg
     });
-    
-    btnNew.addEventListener('click', () => {
-        
-    })
-    
-    
+    btnAsk.disabled = true;
+    btnStop.disabled = true;
+});
+
+// New game
+btnNew.addEventListener('click', () => {
+    deck = [];
+    deck = createDeck();
+    playerPoints = 0;
+    computerPoints = 0;
+    pointsRef[0].innerText = playerPoints;
+    pointsRef[1].innerText = computerPoints;
+    playerCards.innerHTML = '';
+    computerCards.innerHTML = '';
+    btnAsk.disabled = false;
+    btnStop.disabled = false;
+});
