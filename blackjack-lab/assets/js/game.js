664dd5897785c2557ec68f5f6ab85a6e370ed712
Alexandra Salazar <138623509+Alex000127@users.noreply.github.com>
2024-12-04 16:49:51 -0600

    :poop:

--- blackjack-lab/assets/js/game.js
+++ blackjack-lab/assets/js/game.js
@@ -1,138 +1,135 @@
 import '../css/styles.css';
 import _ from 'underscore';
 
+const myModule = (() => {
+    'use strict';
 
+    // Global variables
+    let deck = [];
+    const types = ['C', 'D', 'H', 'S'],
+        special = ['A', 'J', 'Q', 'K'];
+    let playersPoints = [];
+    let msg;
 
-// Global variables
-let deck = [];
-const types = ['C', 'D', 'H', 'S'];
-const special = ['A', 'J', 'Q', 'K'];
-let playerPoints = 0,
-    computerPoints = 0;
-let msg;
+    // HTML references
+    const btnAsk = document.querySelector('#btnAsk'),
+        btnStop = document.querySelector('#btnStop'),
+        btnNew = document.querySelector('#btnNew'),
+        pointsRef = document.querySelectorAll('small'),
+        divCardPlayers = document.querySelectorAll('.divCards');
 
-// HTML references
-const btnAsk = document.querySelector('#btnAsk');
-const btnStop = document.querySelector('#btnStop');
-const btnNew = document.querySelector('#btnNew');
-const pointsRef = document.querySelectorAll('small');
-const playerCards = document.querySelector('#player-cards');
-const computerCards = document.querySelector('#computer-cards');
+    const inicializeGame = (cantPlayers = 2) => {
+        deck = createDeck();
+        playersPoints = Array(cantPlayers).fill(0);
 
-// Create a new deck function
-const createDeck = () => {
-    for (let i = 2; i <= 10; i++) {
+        pointsRef.forEach(elem => elem.innerText = 0);
+        divCardPlayers.forEach(elem => elem.innerHTML = '');
+
+        btnAsk.disabled = false;
+        btnStop.disabled = false;
+    };
+
+    const createDeck = () => {
+        deck = [];
+        for (let i = 2; i <= 10; i++) {
+            for (let type of types) {
+                deck.push(i + type);
+            }
+        }
         for (let type of types) {
-            deck.push(i + type);
+            for (let esp of special) {
+                deck.push(esp + type);
+            }
         }
-    }
+        return _.shuffle(deck);
+    };
 
-    for (let type of types) {
-        for (let esp of special) {
-            deck.push(esp + type);
-        }
-    }
-    deck = _.shuffle(deck);
-    return deck;
-};
+    const askCard = () => {
+        if (deck.length === 0) throw ('No more cards in deck');
+        return deck.pop();
+    };
 
-createDeck();
+    const cardValue = (card) => {
+        const value = card.substring(0, card.length - 1);
+        return isNaN(value) ? (value === 'A' ? 11 : 10) : parseInt(value);
+    };
 
-// Ask for a card function
-const askCard = () => {
-    if (deck.length === 0) throw ('No more cards in deck');
-    return deck.pop();
-};
+    const accumulatePoints = (card, turn) => {
+        playersPoints[turn] += cardValue(card);
+        pointsRef[turn].innerText = playersPoints[turn];
+        return playersPoints[turn];
+    };
 
-// Get card value function
-const cardValue = (card) => {
-    const value = card.substring(0, card.length - 1);
-    return isNaN(value) ? (value === 'A' ? 11 : 10) : parseInt(value);
-};
-
-// Computer's turn, returns a message
-const computerTurn = (minPoints) => {
-    do {
-        const card = askCard();
-        computerPoints += cardValue(card);
-        pointsRef[1].innerText = computerPoints;
-
+    const createCard = (card, turn) => {
         const newCard = document.createElement('img');
         newCard.src = `../cartas/${card}.png`;
         newCard.classList.add('cards');
-        computerCards.append(newCard);
+        divCardPlayers[turn].append(newCard);
+    };
 
-        if (minPoints > 21) break;
+    const determinateWinner = () => {
+        const [minPoints, computerPoints] = playersPoints;
+        if (computerPoints === minPoints) {
+            msg = "It's a tie";
+        } else if (computerPoints > minPoints && computerPoints <= 21) {
+            msg = "Computer wins";
+        } else {
+            msg = "Computer wins";
+        }
+    };
 
-    } while (computerPoints < minPoints && computerPoints <= 21);
+    const computerTurn = (minPoints) => {
+        let computerPoints;
+        do {
+            const card = askCard();
+            computerPoints = accumulatePoints(card, playersPoints.length - 1);
+            createCard(card, playersPoints.length - 1);
+            if (minPoints > 21) break;
+        } while (computerPoints < minPoints && computerPoints <= 21);
+        determinateWinner();
+        return msg;
+    };
 
-    if (computerPoints === minPoints) {
-        msg = "It's a tie";
-    } else if (computerPoints > minPoints && computerPoints <= 21) {
-        msg = "Computer wins";
-    } else {
-        msg = "computer win";
-    }
+    btnAsk.addEventListener('click', () => {
+        const card = askCard();
+        const playerPoints = accumulatePoints(card, 0);
+        createCard(card, 0);
 
-    return msg;
-};
+        if (playerPoints > 21) {
+            btnAsk.disabled = true;
+            btnStop.disabled = true;
+            Swal.fire({
+                icon: "error",
+                title: "You lost...",
+                text: computerTurn(playerPoints)
+            });
+        } else if (playerPoints === 21) {
+            btnAsk.disabled = true;
+            btnStop.disabled = true;
+            Swal.fire({
+                title: "Good job!",
+                text: "You win!",
+                icon: "success"
+            });
+        }
+    });
 
-// Player turn
-btnAsk.addEventListener('click', () => {
-    const card = askCard();
-    playerPoints += cardValue(card);
-    pointsRef[0].innerText = playerPoints;
-
-    const newCard = document.createElement('img');
-    newCard.src = `../cartas/${card}.png`;
-    newCard.classList.add('cards');
-    playerCards.append(newCard);
-
-    if (playerPoints > 21) {
-        btnAsk.disabled = true;
-        btnStop.disabled = true;
-        let showMsg;
-        showMsg = computerTurn(playerPoints);
+    btnStop.addEventListener('click', () => {
+        const [playerPoints] = playersPoints;
+        const showMsg = computerTurn(playerPoints);
         Swal.fire({
-            icon: "error",
-            title: "You lost...",
+            icon: showMsg === "It's a tie" ? "info" : "error",
+            title: showMsg === "It's a tie" ? 'Nobody won' : "You lost...",
             text: showMsg
         });
-    } else if (playerPoints === 21) {
         btnAsk.disabled = true;
         btnStop.disabled = true;
-        Swal.fire({
-            title: "Good job!",
-            text: "You win!",
-            icon: "success"
-        });
-    }
-});
-
-// Stop button
-btnStop.addEventListener('click', () => {
-    let showMsg;
-    showMsg = computerTurn(playerPoints);
-    title = (showMsg === "It's a tie") ? 'Nobody won' : "You lost...";
-    Swal.fire({
-        icon: "error",
-        title: title,
-        text: showMsg
     });
-    btnAsk.disabled = true;
-    btnStop.disabled = true;
-});
 
-// New game
-btnNew.addEventListener('click', () => {
-    deck = [];
-    deck = createDeck();
-    playerPoints = 0;
-    computerPoints = 0;
-    pointsRef[0].innerText = playerPoints;
-    pointsRef[1].innerText = computerPoints;
-    playerCards.innerHTML = '';
-    computerCards.innerHTML = '';
-    btnAsk.disabled = false;
-    btnStop.disabled = false;
-});
+    btnNew.addEventListener('click', () => {
+        inicializeGame();
+    });
+    return {
+        newGame: inicializeGame 
+    };
+})();
